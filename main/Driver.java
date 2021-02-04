238d9369b6029fcb275846a1df72e48e5d1e3d60
Manoj.S <59215525+manojdev21@users.noreply.github.com>
2021-02-04 11:40:31 +0530

    Add files via upload
    Driver class main method updated for testing.

--- main/Driver.java
+++ main/Driver.java
@@ -2,6 +2,7 @@ package main;
 
 import arrays.Array;
 import linkedlist.LinkedList;
+import stack.Stack;
 
 public class Driver {
 
@@ -17,15 +18,36 @@ public class Driver {
 //        array.print();
 
         // LinkedList
-        LinkedList list = new LinkedList();
-        list.insert(18);
-        list.insert(45);
-        list.insert(12);
-        list.insertAtStart(70);
-        list.insertAt(2, 55);
-        list.insertAt(0, 1);
-        list.deleteAt(2);
-        list.show();
+//        LinkedList list = new LinkedList();
+//        list.insert(18);
+//        list.insert(45);
+//        list.insert(12);
+//        list.insertAtStart(70);
+//        list.insertAt(2, 55);
+//        list.insertAt(0, 1);
+//        list.deleteAt(2);
+//        list.show();
+
+        // Stack
+//        Stack stack = new Stack();
+//        stack.push(10);
+//        stack.push(20);
+//        stack.push(30);
+//        stack.push(40);
+//        stack.push(55);
+//        stack.show();
+//        System.out.println("Stack size: " + stack.size());
+//        System.out.println("Stack is empty: " + stack.isEmpty());
+//        System.out.println("Peek element: " + stack.peek());
+//
+//        System.out.println("---------");
+//
+//        System.out.println("Popped element: " + stack.pop());
+//        System.out.println("Popped element: " + stack.pop());
+//        System.out.println("Stack size: " + stack.size());
+//        System.out.println("Stack is empty: " + stack.isEmpty());
+//        System.out.println("Peek element: " + stack.peek());
+//        stack.show();
 
     }
 
